e4ebc9add287f089db3af95a3c7deb6cad701463
csschan <125574923@qq.com>
2020-02-25 21:03:30 +0800

    merkletest

---
+++ src/bench.rs
@@ -0,0 +1,22 @@
+#![cfg(test)]
+
+
+extern crate test;
+
+use self::test::Bencher;
+
+use std::iter;
+use super::MerkleTree;
+
+#[bench]
+fn build(b: &mut Bencher) {
+    let block = "Hello World";
+    let blocks: Vec<&str> = iter::repeat(block).take(100).collect();
+
+    b.iter(|| {
+        let n = test::black_box(100);
+        for _i in 0..n {
+            let _t: MerkleTree = MerkleTree::build(blocks.as_slice());
+        }
+    });
+}
